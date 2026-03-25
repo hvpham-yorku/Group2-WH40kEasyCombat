b88274fe7aeb6735ba221248dcc51da3090d012e
HenryCROSS <37565534+HenryCROSS@users.noreply.github.com>
2026-03-25 18:32:53 -0400

    Henry customized cal sys (#78)
    add vm for customizable rules

--- src/main/java/eecs2311/group2/wh40k_easycombat/Main.java
+++ src/main/java/eecs2311/group2/wh40k_easycombat/Main.java
@@ -1,9 +1,12 @@
 package eecs2311.group2.wh40k_easycombat;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.sql.SQLException;
+import java.util.Arrays;
 
 import eecs2311.group2.wh40k_easycombat.db.Database;
+import eecs2311.group2.wh40k_easycombat.service.vm.VMService;
 import eecs2311.group2.wh40k_easycombat.util.CsvToSqliteImporter;
 import eecs2311.group2.wh40k_easycombat.util.FixedAspectView;
 import javafx.application.Application;
@@ -27,6 +30,7 @@ public class Main extends Application {
         Database.generateSchemaFile();
         Database.executeSqlFolder("src/main/resources/sql/");
         System.out.println("SQL scripts executed!");
+        VMService.loadFolder(Path.of("src/main/resources/dsl/"));
 
         CsvToSqliteImporter.importDefaultCsvSeedLike(false);
 
