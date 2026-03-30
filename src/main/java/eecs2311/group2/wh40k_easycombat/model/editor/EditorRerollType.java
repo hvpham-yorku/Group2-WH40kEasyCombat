ea63e6f099353958bb747f1ce3db9f0c38052bdb
HenryCROSS <henry583050973@gmail.com>
2026-03-29 23:18:03 -0400

    improvement

--- src/main/java/eecs2311/group2/wh40k_easycombat/model/editor/EditorRerollType.java
+++ src/main/java/eecs2311/group2/wh40k_easycombat/model/editor/EditorRerollType.java
@@ -1,20 +1,22 @@
 package eecs2311.group2.wh40k_easycombat.model.editor;
 
 public enum EditorRerollType {
-    NONE("None"),
-    ONES("Re-roll 1s"),
-    FAILS("Re-roll Fails");
+    NONE("None", 0),
+    ONES("Re-roll 1s", 1),
+    FAILS("Re-roll Fails", 2);
 
     private final String label;
+    private final int power;
 
-    EditorRerollType(String label) {
+    EditorRerollType(String label, int power) {
         this.label = label;
+        this.power = power;
     }
 
     public static EditorRerollType stronger(EditorRerollType left, EditorRerollType right) {
         EditorRerollType safeLeft = left == null ? NONE : left;
         EditorRerollType safeRight = right == null ? NONE : right;
-        return safeLeft.ordinal() >= safeRight.ordinal() ? safeLeft : safeRight;
+        return safeLeft.power >= safeRight.power ? safeLeft : safeRight;
     }
 
     @Override
