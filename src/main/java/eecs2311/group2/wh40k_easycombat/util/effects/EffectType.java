b81d7409fcc22b623c4115c4137b4e64c177db56
SanK <sanzuhair2006@gmail.com>
2026-03-30 14:22:13 -0400

    Update EffectType.java

--- src/main/java/eecs2311/group2/wh40k_easycombat/util/effects/EffectType.java
+++ src/main/java/eecs2311/group2/wh40k_easycombat/util/effects/EffectType.java
@@ -1,3 +1,10 @@
+//Comments:
+// Enumeration that categorizes effects based on their general purpose.
+//
+// EffectType provides a high-level classification (e.g., DAMAGE, HEAL, SPEED), which can be used for organizing, filtering, or identifying effects.
+//
+// While Tag defines what is modified, EffectType describes the nature or theme of the effect itself.
+
 package eecs2311.group2.wh40k_easycombat.util.effects;
 
 public enum EffectType {
